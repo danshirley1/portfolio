2c79140baf3571b16a8ab00a564fb1d6be684493
Dan Shirley <danshirley@Dans-MacBook-Pro.local>
2017-08-16 16:52:27 +0100

    Fixed test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+
+import App from './containers/App';
+import store, { history } from './store/'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render((
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <div>
+          <App />
+        </div>
+      </ConnectedRouter>
+    </Provider>
+  ), div);
 });
+
